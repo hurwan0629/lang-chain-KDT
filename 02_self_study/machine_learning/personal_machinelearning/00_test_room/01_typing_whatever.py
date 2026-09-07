a93c60fba115729c17e7d15a4cd6617510e18ab7
hurwan0629 <hurwan0629@gmail.com>
2026-09-07 22:02:45 +0900

    AlexNet, DenseNet, VGG(진행중) 논문 & refactoring

---
+++ 02_self_study/machine_learning/personal_machinelearning/00_test_room/01_typing_whatever.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            # Conv
+            # BN
+            # ReLU
+            # Pool
+
+            # Conv
+            # BN
+            # ReLU
+            # Pool
+        )
